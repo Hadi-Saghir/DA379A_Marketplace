1e78296ead3fabf4c3e961cac9adb57877e66f95
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2023-05-08 20:41:24 +0200

    move directories

--- Server/src/StartServer.java
+++ Server/src/StartServer.java
@@ -1,5 +1,7 @@
+import ServerController;
 public class StartServer {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ServerController serverController = new ServerController();
     }
 }
