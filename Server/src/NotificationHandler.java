42ec34d205c64b286f5a98b8e62b04bcec212189
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2023-05-08 20:49:06 +0200

    created modules instead of directories for server and client

--- Server/src/NotificationHandler.java
+++ Server/src/NotificationHandler.java
@@ -1,5 +1,3 @@
-package Notifcation;
-
 import java.util.Observable;
 import java.util.Observer;
 
@@ -7,7 +5,7 @@ import java.util.Observer;
 /**
  * This class will apply the observer software design pattern
  * The purpose of this class is to generate observe for events that trigger a notification*/
-public class Notification implements Observer {
+public class NotificationHandler implements Observer {
 
     @Override
     public void update(Observable o, Object arg) {
--- Server/src/StartServer.java
+++ Server/src/StartServer.java
@@ -1,7 +1,5 @@
-import ServerController;
 public class StartServer {
     public static void main(String[] args) {
-
         ServerController serverController = new ServerController();
     }
 }
