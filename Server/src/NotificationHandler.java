41b5e79bd1f79c30ecb170bb4d4d6b802e3f903f
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2023-05-09 13:05:19 +0200

    notificationhandler update

--- Server/src/NotificationHandler.java
+++ Server/src/NotificationHandler.java
@@ -1,11 +1,15 @@
 import Handlers.Client;
 import Handlers.ClientHandler;
+import Handlers.RequestHandler;
 import Shared.Notification;
+import Shared.Request;
 
 import java.io.ObjectOutputStream;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 
 /**
@@ -13,9 +17,14 @@ import java.util.Observer;
  * The purpose of this class is to generate observe for events that trigger a notification*/
 public class NotificationHandler implements Observer {
     private Map<Client, ClientHandler> onlineClients;
+    private Queue<Notification> notifications;
 
-    public NotificationHandler(Map<Client, ClientHandler> onlineClients) {
+    private RequestHandler requestHandler;
 
+    public NotificationHandler(Map<Client, ClientHandler> onlineClients, RequestHandler requestHandler) {
+        this.onlineClients = onlineClients;
+        this.notifications = new ConcurrentLinkedQueue<>();
+        this.requestHandler = requestHandler;
     }
 
     public void addNewClient(Client client, ClientHandler clientHandler){
@@ -26,34 +35,34 @@ public class NotificationHandler implements Observer {
         onlineClients.remove(client);
     }
 
+    public void addNotification(Notification notification) {
+        notifications.add(notification);
+    }
+
     @Override
     public void update(Observable o, Object arg) {
         if (arg instanceof Notification) {
             Notification notification = (Notification) arg;
+            addNotification(notification);
+        }
+    }
+
+    public void processNotifications() {
+        while (!notifications.isEmpty()) {
+            Notification notification = notifications.poll();
             String clientId = notification.getClientId();
             String message = notification.getMessage();
 
-            ObjectOutputStream outputStream;
-
-            outputStream = new ObjectOutputStream();
-
-            synchronized (outputStreams) {
-                outputStream = outputStreams.get(clientId);
+            ClientHandler clientHandler;
+            synchronized (onlineClients) {
+                clientHandler = onlineClients.get(new Client(clientId));
             }
 
-            if (outputStream != null) {
-                // lock the output stream while writing to it
-                synchronized (outputStream) {
-                    try {
-                        outputStream.writeObject(new NotificationMessage(message));
-                        outputStream.flush();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+            if (clientHandler != null) {
+                clientHandler.writeObject(new NotificationMessage(message));
             } else {
                 // client not found, add notification to database
-                DBHandler.addNotification(clientId, message);
+                requestHandler.handleRequest();
             }
         }
     }
